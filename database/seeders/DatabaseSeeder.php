d612fd39374f5f07831752bd502baba9787ea83d
QuangHieu0711 <nguyenquanghieugl2019@gmail.com>
2024-12-21 21:28:22 +0700

    Update DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,9 +18,5 @@ class DatabaseSeeder extends Seeder
             TaskSeeder::class,
         ]);
         
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
     }
 }
